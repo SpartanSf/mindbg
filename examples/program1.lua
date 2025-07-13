c9642e355cfcf29ab277d2bfa6b380c4eec6fc8b
SpartanSoftware <swoshswosh@proton.me>
2025-07-13 12:50:33 +1200

    Create program1.lua

---
+++ examples/program1.lua
@@ -0,0 +1,5 @@
+-- Keep entering "s" until you reach "local x = 10". Note that there is an "until" command in the debugger for larger programs.
+local x = 10 -- When you see this on the [DEBUG] line, enter s one more time to execute the line.
+local y = x + 5 -- When you see this on the [DEBUG] line, x has been defined. Run "print x" to see the value of x.
+local z = y * 2
+print("y =", y) -- When you see this on the [DEBUG] line, enter s one more to execute the line. It will print on the left side of the console, where program output goes.
